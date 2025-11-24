5a97bcd0e3cf39cbb5b44f3b545d8c2dbe8dd9e4
J0pi3 <joeltenbrink@kpnmail.nl>
2025-11-24 14:34:37 +0100

    initial demo code
    working demo code

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <avr/delay.h>
+#include <NunchuckControl.h>
+#include <SPI.h>
+
+// PD6 --> led
+// PD2 --> sensor
+
+void InitializeIO()
+{
+  initializeIRIO();
+
+  // INT0 falling edge
+  EICRA |= (1 << ISC01);
+  EICRA &= ~(1 << ISC00);
+  EIMSK |= (1 << INT0);
+
+  sei();
+}
+
+int main(void)
+{
+  InitializeIO();
+  init();
+  Wire.begin();
+  Nunchuk.begin(NUNCHUK_ADDRESS);
+  tft.begin();
+  clearScreen();
+  
+  InitializeIO();
+  while(true)
+  {
+    nunchukControl();
+  }
+  return 0;
+}
